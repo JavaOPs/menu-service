cb258d1782fc7f835124d0ebfefc9b412050bbb0
JavaOPs <admin@javaops.ru>
2024-05-09 00:29:56 +0300

    Initial setup

---
+++ src/main/java/ru/javaops/cloudjava/menuservice/MenuServiceApplication.java
@@ -0,0 +1,13 @@
+package ru.javaops.cloudjava.menuservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MenuServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MenuServiceApplication.class, args);
+	}
+
+}
